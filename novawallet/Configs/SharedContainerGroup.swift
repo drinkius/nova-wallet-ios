9d2145e8ecec629baba742616fe684724e602d33
Alex Telegin <telegin.alexander@gmail.com>
2024-09-19 14:27:03 +0300

    Temporary project updates for compilation and tests

--- novawallet/Configs/SharedContainerGroup.swift
+++ novawallet/Configs/SharedContainerGroup.swift
@@ -3,9 +3,9 @@ import Foundation
 enum SharedContainerGroup {
     static var name: String {
         #if F_RELEASE
-            return "group.novafoundation.novawallet"
+            return "group.novafoundation.novawallettel"
         #else
-            return "group.novafoundation.novawallet.dev"
+            return "group.novafoundation.novawallettel"
         #endif
     }
 }
